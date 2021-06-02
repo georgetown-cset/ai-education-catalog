738f54bf7c3d3a9266bf15891e477218071c2a29
Jennifer Melot <jtmelot@gmail.com>
2021-06-02 18:38:09 -0400

    Add config files for webapp, initial snapshots of raw data, and beginning of reformatting script

---
+++ preprocessing/reformat_data.py
@@ -0,0 +1,69 @@
+import argparse
+import csv
+import os
+import us
+
+
+def reformat_data(raw_data_dir: str, output_dir: str) -> None:
+    cleaned_data = []
+    for fi in os.listdir(raw_data_dir):
+        row_gen = get_non_comment_rows(os.path.join(raw_data_dir, fi))
+        for line in csv.DictReader(row_gen):
+            cost = "Free" if line.get("Cost", "").strip().lower() == "free" else "Not Free"
+            locations = clean_locations(line.get("Location"))
+            special_focus = get_special_focus(line)
+            cleaned_data.append({
+                "name": line["Program"],
+                "type": line["Type"],
+                "organization": line.get("Organization"),
+                "target": line.get("Target", "").split(),
+                "cost": cost,
+                "location": locations,
+                "underrep": special_focus
+            })
+
+
+def get_special_focus(line: dict) -> list:
+    foci = []
+    for key in ["Underrepresented", "Community", "Gender"]:
+        if key in line:
+            foci.append(line[key])
+    return foci
+
+
+def clean_locations(location: str) -> list:
+    if location is None:
+        return []
+    clean_locations = []
+    for loc in location.strip().split(","):
+        if len(loc) == 2:
+            loc = us.states.lookup(loc)
+        clean_locations.append(loc)
+    return clean_locations
+
+
+def get_non_comment_rows(filename: str) -> iter:
+    name_to_comment_rows = {
+        "after_school_programs.csv": 4,
+        "conferences_challenges.csv": 2,
+        "curriculum.csv": 3,
+        "federal_initiatives.csv": 2,
+        "scholarships.csv": 2,
+        "summer_camps.csv": 3
+    }
+    num_commment_rows = name_to_comment_rows[os.path.basename(filename)]
+    with open(filename) as f:
+        for line in f:
+            if num_commment_rows > 0:
+                num_commment_rows -= 1
+                continue
+            yield line
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--raw_data_dir", default="raw_data")
+    parser.add_argument("--output_dir", default="../ai-education-programs/src/data")
+    args = parser.parse_args()
+
+    reformat_data(args.raw_data_dir, args.output_dir)
