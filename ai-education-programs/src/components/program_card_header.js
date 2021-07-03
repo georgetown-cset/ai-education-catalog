88d56e880d0e0ff4eff55dab48c6eac36d1884fc
Jennifer Melot <jtmelot@gmail.com>
2021-07-03 09:51:30 -0400

    Add cost to card header, remove sub-header

--- ai-education-programs/src/components/program_card_header.js
+++ ai-education-programs/src/components/program_card_header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import Typography from "@material-ui/core/Typography";
-import LocationOnIcon from '@material-ui/icons/LocationOn';
 import "../styles/styles.css";
 
 const ProgramCardHeader = (props) => {
@@ -26,18 +25,8 @@ const ProgramCardHeader = (props) => {
     <div>
         <CardActionArea style={{backgroundColor: color+",1)", padding: "7px 5px", color: "white", textAlign: "center"}}>
           <Typography variant={"body2"} style={{fontWeight: "bold"}}>
-            {programEmoji[program.type]} {program.type}
+            {programEmoji[program.type]} {program.type} - {program.cost}
           </Typography>
-        </CardActionArea>
-        <CardActionArea style={{backgroundColor: color+",0.7)", padding: "3px 5px", color: "white", textAlign: "center"}}>
-          <div>
-            <div style={{display: "inline-block", minWidth: "100px", width: "50%", textAlign: "center", borderRight: "1px solid white"}}>
-              <Typography variant={"body2"} style={{fontSize: "80%", fontWeight: "bold"}}>{program.cost}</Typography>
-            </div>
-            <div style={{display: "inline-block", minWidth: "100px", width: "50%", textAlign: "center"}}>
-              <Typography variant={"body2"} style={{fontSize: "80%", fontWeight: "bold"}}><LocationOnIcon style={{fontSize: "120%", verticalAlign: "top"}}/> {program.location.join(", ")}</Typography>
-            </div>
-          </div>
         </CardActionArea>
     </div>
   )
