42bb2d6ea67961ef2b3ce11549f26fe6438edaa8
Jennifer Melot <jtmelot@gmail.com>
2021-07-02 21:55:53 -0400

    Use location icon, remove "Cost" label

--- ai-education-programs/src/components/program_card_header.js
+++ ai-education-programs/src/components/program_card_header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import Typography from "@material-ui/core/Typography";
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 import "../styles/styles.css";
 
 const ProgramCardHeader = (props) => {
@@ -31,10 +32,10 @@ const ProgramCardHeader = (props) => {
         <CardActionArea style={{backgroundColor: color+",0.7)", padding: "3px 5px", color: "white", textAlign: "center"}}>
           <div>
             <div style={{display: "inline-block", minWidth: "100px", width: "50%", textAlign: "center", borderRight: "1px solid white"}}>
-              <Typography variant={"body2"} style={{fontSize: "80%", fontWeight: "bold"}}>Cost: {program.cost}</Typography>
+              <Typography variant={"body2"} style={{fontSize: "80%", fontWeight: "bold"}}>{program.cost}</Typography>
             </div>
             <div style={{display: "inline-block", minWidth: "100px", width: "50%", textAlign: "center"}}>
-              <Typography variant={"body2"} style={{fontSize: "80%", fontWeight: "bold"}}>Location: {program.location.join(", ")}</Typography>
+              <Typography variant={"body2"} style={{fontSize: "80%", fontWeight: "bold"}}><LocationOnIcon style={{fontSize: "120%", verticalAlign: "top"}}/> {program.location.join(", ")}</Typography>
             </div>
           </div>
         </CardActionArea>
