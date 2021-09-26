b2b432072b71193e45d5cd4edf597605983e815d
Jennifer Melot <jtmelot@gmail.com>
2021-09-26 13:16:56 -0400

    Remove unused function

--- ai-education-programs/src/components/program_card_area.js
+++ ai-education-programs/src/components/program_card_area.js
@@ -228,23 +228,6 @@ const ProgramCardArea = (props) => {
     window.history.replaceState(null, null, window.location.pathname);
   };
 
-  function stateLocationSelected(selectedLocations){
-    // We may eventually have some non-state locations that aren't USA or Virtual, so we have to enumerate these
-    const states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
-      "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
-      "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
-      "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
-      "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
-      "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming",
-      "District of Columbia", "Puerto Rico"];
-    for(let loc of selectedLocations){
-      if(states.includes(loc)){
-        return true;
-      }
-    }
-    return false;
-  }
-
   return (
     <div style={{backgroundColor: "#FFFFFF", textAlign: "right"}}>
       <Paper id={"search-bar"} elevation={2} style={{paddingBottom: "10px"}}>
