a9070e0e158f14ae56a0d9545679365ef0658428
Jennifer Melot <jtmelot@gmail.com>
2021-09-27 19:45:04 -0400

    Remove unused markdown template and merge stylesheets

--- ai-education-programs/src/markdown-pages/about.md
+++
@@ -1,8 +0,0 @@
----
-slug: "/about"
-title: "About the AI Education Catalog"
----
-
-Here is some information about the project.
-
-More information about the project.
--- ai-education-programs/src/pages/index.js
+++ ai-education-programs/src/pages/index.js
@@ -4,7 +4,6 @@ import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
 
 import "../styles/styles.css";
-import "../components/layout.css"
 import header1 from "../images/header1.png";
 import header2 from "../images/header2.png";
 import header3 from "../images/header3.png";
