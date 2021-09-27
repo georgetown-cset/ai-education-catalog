9581855b1dce000a116dd7706b2e96abcd545fe5
Jennifer Melot <jtmelot@gmail.com>
2021-09-27 11:09:03 -0400

    Add background images

--- ai-education-programs/src/components/toolbar.js
+++ ai-education-programs/src/components/toolbar.js
@@ -4,17 +4,17 @@ import React from "react";
 
 const CatalogToolbar = () => {
   return (
-    <div id="toolbar" style={{"margin": "20px 20px 0px 20px"}}>
-      <div style={{width: "50%", display: "inline-block", verticalAlign: "top"}}>
+    <div id="toolbar" style={{padding: "15px 20px 10px 20px", backgroundColor: "rgb(21, 32, 74)"}}>
+      <div style={{width: "50%", display: "inline-block", verticalAlign: "middle"}}>
         <a href={"https://cset.georgetown.edu"} target="_blank" rel="noreferrer"
           title="Link to CSET website, cset.georgetown.edu">
-          <img src={cset_logo} style={{"width": "300px"}} alt="CSET Logo"/>
+          <img src={cset_logo} style={{width: "300px", margin: "0"}} alt="CSET Logo"/>
         </a>
       </div>
-      <div style={{width: "50%", display: "inline-block", textAlign: "right", verticalAlign: "top"}}>
+      <div style={{width: "50%", display: "inline-block", textAlign: "right", verticalAlign: "middle"}}>
         <a href={"https://aiedu.org/"} target="_blank" rel="noreferrer"
           title="Link to AI Education Project website, aiedu.org">
-          <img src={aiedu_logo} style={{"height": "50px"}} alt="AI Edu Logo"/>
+          <img src={aiedu_logo} style={{height: "40px", margin: "0"}} alt="AI Edu Logo"/>
         </a>
       </div>
     </div>
--- ai-education-programs/src/pages/index.js
+++ ai-education-programs/src/pages/index.js
@@ -4,6 +4,11 @@ import Link from '@material-ui/core/Link';
 
 import "../styles/styles.css";
 import "../components/layout.css"
+import header1 from "../images/header1.png";
+import header2 from "../images/header2.png";
+import header3 from "../images/header3.png";
+import header4 from "../images/header4.png";
+import header5 from "../images/header5.png";
 import CatalogToolbar from "../components/toolbar";
 
 const ProgramCardArea = React.lazy(() => import("../components/program_card_area"));
@@ -28,10 +33,19 @@ const IndexPage = () => {
   return (
     <div>
       <CatalogToolbar/>
-      <div id="project-description" style={{"margin": "0px 10% 50px 10%", textAlign: "center"}}>
-        <h1>🤖 AI Education Catalog 🤖</h1>
-        <h4>A joint project from the <Link>Center for Security and Emerging Technology</Link> and the <Link>AI Education Project</Link>.</h4>
-        <h4><Link style={{padding: "0px 10px"}} href={"/about"}>About</Link> • <Link style={{padding: "0px 5px"}} href={"/team"}>Team</Link> • <Link style={{padding: "0px 5px"}}>Contact Us</Link></h4>
+      <div id="colorbar-container">
+        <div id="colorbar">
+          <img alt="header1" src={header1} id={"header1"} className={"header-img"}/>
+          <img alt="header2" src={header2} id={"header2"} className={"header-img"}/>
+          <img alt="header3" src={header3} id={"header3"} className={"header-img"}/>
+          <img alt="header4" src={header4} id={"header4"} className={"header-img"}/>
+          <img alt="header5" src={header5} id={"header5"} className={"header-img"}/>
+        </div>
+        <div id="project-description" style={{"margin": "0px 10% 50px 10%", textAlign: "center", zIndex: 2, color: "white", position: "absolute", top: "0px"}}>
+          <h1>🤖 AI Education Catalog 🤖</h1>
+          <h4>A joint project from the <Link>Center for Security and Emerging Technology</Link> and the <Link>AI Education Project</Link>.</h4>
+          <h4><Link style={{padding: "0px 10px"}} href={"/about"}>About</Link> • <Link style={{padding: "0px 5px"}} href={"/team"}>Team</Link> • <Link style={{padding: "0px 5px"}}>Contact Us</Link></h4>
+        </div>
       </div>
       {!isSSR && (
         <React.Suspense fallback={<div style={{textAlign: "center"}}><CircularProgress/></div>}>
