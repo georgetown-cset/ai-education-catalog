9e59f6459348b4bc035508012b147413b36d7d36
Jennifer Melot <jtmelot@gmail.com>
2021-09-29 09:39:21 -0400

    Update package metadata

--- ai-education-catalog/gatsby-config.js
+++ ai-education-catalog/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `AI Education Catalog`,
+    description: `Catalog of AI-related education programs`,
+    author: `CSET and AI Education`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
